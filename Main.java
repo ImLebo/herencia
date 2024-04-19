99ae752a24e1b5d6c8416c155d4b3cef5533b921
luisfb12b@gmail.com <luisfb12b@gmail.com>
2024-04-18 19:49:22 -0500

    Modify the Main archive

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Student student = new Student("Chimbi dimbi", "Agogo", "1234", "Helicoptero", "777");
+        Student student = new Student("Kevin Lopez", "Agogo", "1234", "Helicoptero", "777");
 
         System.out.println(student.toString());
     }
